346cba89ebfc8a726a75f58e7ba2513eb6ea791b
ctx2002 <ctx2002@gmail.com>
2017-09-20 10:04:50 +1200

    made a comment about quantity kind

--- src/QUDV/Quantity/Converter/NZD2AUD.php
+++ src/QUDV/Quantity/Converter/NZD2AUD.php
@@ -3,6 +3,10 @@ namespace QUDV\Quantity\Converter;
 
 use QUDV\Quantity\Converter\Converter;
 use QUDV\Quantity\NZD;
+
+/**
+ this conversion is wrong, we have not consider QuantityKind yet
+***/
 class NZD2AUD extends Converter
 {
     private $conversion;
